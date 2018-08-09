3af98c54d96b728385d7ee80a60e72cf466a4b14
Dimitris Bliamplias <bl.dimitris@gmail.com>
2018-08-09 17:39:03 +0300

    client: Add a basic module to interact with HAProxy
    The `client.go` module, implements the package's main API with HAProxy's
    UNIX Socket interface.
    
    It consists of the `HAProxyClient` type that is the main component of
    the current module and corresponds to a HAProxy connection object. In
    order to send a command to HAProxy you should use the `SendCommand`
    function. The command argument should be in string format. As for the
    return values, we currently don't perform any kind of sanity check in
    the server's response; we just convert it to string and return it to the
    caller.
    
    Finally, note that currently you can only interact with HAProxy via a
    UNIX socket. An upcoming patch will provide support for listening to a
    TCP socket, too.

---
+++ haproxy/client.go
@@ -0,0 +1,59 @@
+/*
+
+  Module implementing the API with HAProxy's UNIX Socket interface.
+
+*/
+
+package haproxy
+
+import "fmt"
+import "io/ioutil"
+import "net"
+import "strings"
+import "time"
+
+// A HAProxyClient instance represents a HAProxy connection object.
+type HAProxyClient struct {
+	AfNet   string
+	Address string
+	Timeout time.Duration
+	conn    net.Conn
+}
+
+// Connects to the address on the given network type.
+// The library can listen to a either a TCP port or a UNIX socket.
+func (h *HAProxyClient) connect() (err error) {
+	switch h.AfNet {
+	case AFNET_UNIX:
+		h.conn, err = net.DialTimeout(h.AfNet, h.Address, h.Timeout)
+	case AFNET_TCP:
+		err = fmt.Errorf("Not implemented, yet!")
+	default:
+		err = fmt.Errorf("Supported network types are '%s', and '%s'\n", AFNET_UNIX, AFNET_TCP)
+	}
+	return err
+}
+
+// Send the given command to HAProxy over the UNIX admin socket.
+func (h *HAProxyClient) SendCommand(cmd string) (string, error) {
+	if err := h.connect(); err != nil {
+		return EMPTY_RESP, err
+	}
+	defer h.conn.Close()
+
+	//. ensure a newline char will be always appended to the command
+	command := []byte(strings.Replace(cmd, "\n", "", -1) + "\n")
+
+	h.conn.SetWriteDeadline(time.Now().Add(h.Timeout))
+	if _, err := h.conn.Write(command); err != nil {
+		return EMPTY_RESP, err
+	}
+
+	h.conn.SetReadDeadline(time.Now().Add(h.Timeout))
+	response, err := ioutil.ReadAll(h.conn)
+	if err != nil {
+		return EMPTY_RESP, err
+	}
+
+	return string(response), nil
+}
---
+++ haproxy/constants.go
@@ -0,0 +1,13 @@
+/*
+
+  Module containing the client's constant definitions.
+
+*/
+package haproxy
+
+const (
+	EMPTY_RESP = ""
+
+	AFNET_TCP  = "tcp"
+	AFNET_UNIX = "unix"
+)
